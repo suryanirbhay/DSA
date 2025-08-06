5bcfd7fda921ade5d0a51068286ef71a595aa966
suryanirbhay <suryanirbhay34@gmail.com>
2025-08-07 00:01:02 +0530

    Time: 8 ms (31.97%) | Memory: 9.5 MB (92.98%) - LeetSync

---
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+        string temp = a;
+        int count = 1;
+
+        while (temp.length() < b.length()) {
+            temp += a;
+            count++;
+        }
+
+     
+        if (temp.find(b) != string::npos) {
+            return count;
+        }
+
+       
+        temp += a;
+        count++;
+
+        if (temp.find(b) != string::npos) {
+            return count;
+        }
+
+       
+        return -1;
+    }
+};
